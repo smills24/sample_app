47751f0256687843567afa8bf347b662677316f3
Steph <stephmills24@gmail.com>
2011-04-19 19:09:59 -0400

    Made some pages

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe PageController do
+  render_views
 
   describe "GET 'about'" do
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => " | About")
+    end
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -11,6 +16,11 @@ describe PageController do
 
 
   describe "GET 'home'" do
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => " | Home")
+    end
+
     it "should be successful" do
       get 'home'
       response.should be_success
@@ -18,6 +28,11 @@ describe PageController do
   end
 
   describe "GET 'contact'" do
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => " | Contact")
+    end
+
     it "should be successful" do
       get 'contact'
       response.should be_success
