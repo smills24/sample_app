877fd90d0374eb55f41284d1c75b691a66d2ba42
Steph <stephmills24@gmail.com>
2011-04-21 13:46:36 -0400

    CSS

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -6,17 +6,15 @@ class PageController < ApplicationController
 
   def home
     @title = "Home"
-    @content = "Welcome to the homepage"
   end
 
   def contact
     @title = "Contact"
-    @content = "Email me at stephmills24@gmail.com"
+
   end
 
   def about
     @title = "About"
-    @content = "This is a silly little app I made"   
   end
 
 end
